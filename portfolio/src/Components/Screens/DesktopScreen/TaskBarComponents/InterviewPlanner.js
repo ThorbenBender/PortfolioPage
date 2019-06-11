c5dad75b2db40bf2f72987c1e44caa935c975a45
Thorben Bender <bender.thorben0@gmail.com>
2019-06-11 21:43:08 +0200

    interviewplanner layout done

---
+++ portfolio/src/Components/Screens/DesktopScreen/TaskBarComponents/InterviewPlanner.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function InterviewPlanner() {
+  return (
+    <div>
+      <button>
+        <img />
+        <p>Interview</p>
+      </button>
+    </div>
+  );
+}
+
+export default InterviewPlanner;
