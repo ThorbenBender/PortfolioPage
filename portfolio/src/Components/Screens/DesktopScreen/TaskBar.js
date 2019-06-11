0f02d9415b049e04e9afb6de57e7624ed240a7ea
Thorben Bender <bender.thorben0@gmail.com>
2019-06-11 21:12:54 +0200

    done with taskbar layout

--- portfolio/src/Components/Screens/DesktopScreen/TaskBar.js
+++ portfolio/src/Components/Screens/DesktopScreen/TaskBar.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function TaskBar() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <WindowsButton />
+      <InterviewPlanner />
+      <ContactMe />
+      <Time />
+    </div>
+  );
 }
 
 export default TaskBar;
