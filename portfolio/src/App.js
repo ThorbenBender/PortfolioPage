607f9c2caac2c9907545034abb1458c145f395ad
Thorben Bender <bender.thorben0@gmail.com>
2019-06-11 20:50:01 +0200

    deleted mainscreen

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import store from "./Store/Store";
-import { Provider } from "react-redux";
-import MainScreen from "./Components/Screens/MainScreen";
 
 function App() {
   return (
-    <Provider store={store}>
-      <MainScreen />
-    </Provider>
+    <div>
+      <p>Hello</p>
+    </div>
   );
 }
 
