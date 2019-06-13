9000c07d1f064aad3f62f586236fc14e8751ef29
Thorben Bender <bender.thorben0@gmail.com>
2019-06-13 19:12:47 +0200

    done with pop time

--- portfolio/src/Components/Screens/DesktopScreen/TaskBarComponents/TimeDisplay.js
+++ portfolio/src/Components/Screens/DesktopScreen/TaskBarComponents/TimeDisplay.js
@@ -1,9 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import Popup from "reactjs-popup";
 
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 class TimeDisplay extends React.Component {
   state = {
-    time: new Date().toLocaleTimeString().replace(/(.*)\D\d+/, "$1")
+    time: new Date().toLocaleTimeString().replace(/(.*)\D\d+/, "$1"),
+    showDate: false,
+    date: days[new Date().getDay()] + ", " + new Date().toLocaleDateString()
   };
   componentDidMount() {
     this.intervalID = setInterval(() => this.tick(), 1000);
@@ -13,14 +17,45 @@ class TimeDisplay extends React.Component {
   }
   tick() {
     this.setState({
-      time: new Date().toLocaleTimeString().replace(/(.*)\D\d+/, "$1")
+      time: new Date().toLocaleTimeString().replace(/(.*)\D\d+/, "$1"),
+      date: days[new Date().getDay()] + ", " + new Date().toLocaleDateString()
     });
   }
+
+  showDate = () => {
+    this.setState({ showDate: true });
+  };
+
+  hideDate = () => {
+    this.setState({ showDate: false });
+  };
 
   render() {
     return (
       <StyledTimeDisplay>
-        <StyledTime>{this.state.time}</StyledTime>
+        <Popup
+          trigger={
+            <StyledTime onMouseEnter={this.showDate} onMouseLeave={this.hideDate}>
+              {this.state.time}
+            </StyledTime>
+          }
+          position="top center"
+          on="hover"
+          mouseLeaveDelay={200}
+          mouseEnterDelay={0}
+          contentStyle={{
+            padding: "0px",
+            height: "2.5vh",
+            display: "flex",
+            alignItems: "center",
+            width: "6.5vw",
+            marginBottom: "3vh",
+            fontSize: ".9rem"
+          }}
+          offsetY="12"
+          arrow={false}>
+          {!this.state.showDate && <StyledFullDate>{this.state.date}</StyledFullDate>}
+        </Popup>
       </StyledTimeDisplay>
     );
   }
@@ -29,16 +64,12 @@ class TimeDisplay extends React.Component {
 export default TimeDisplay;
 
 const StyledTimeDisplay = styled.div`
-  position: fixed;
-  right: 1vw;
-  border-left-style: ridge;
-  border-left-color: lightgray;
-  height: 2vh;
-  vertical-align: center;
-  padding-left: 1vw;
-  /* bottom: 2.45vh; */
+  margin-left: 87vw;
 `;
 
-const StyledTime = styled.p`
-  margin: 0;
+const StyledTime = styled.p``;
+
+const StyledFullDate = styled.p`
+  display: flex;
+  align-items: center;
 `;
